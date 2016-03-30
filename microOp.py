853ba9421260da9659c8b86d3aeafdb7ae35c026
garzon <610204679@qq.com>
2016-03-30 14:03:28 +0800

    with an unfinished example of my instruction set

--- microOp.py
+++ microOp.py
@@ -3,10 +3,17 @@ genOpcode = []
 defaultOpcode = '\xfe\x7f\xff\xff'
 currentOpcode = defaultOpcode
 
+currentBeats = 0
+
+def bitAnd(a, b):
+   return ''.join([chr(ord(c)&ord(d)) for c, d in zip(a,b)])
+
 def xor(a, b):
    return ''.join([chr(ord(c)^ord(d)) for c, d in zip(a,b)])
    
 def globalVarInPlaceXor(globalVarName, xor_op):
+    global defaultOpcode
+    assert(xor(xor(bitAnd(globals()[globalVarName], xor_op), xor_op), defaultOpcode) != '\x00\x00\x00\x00')
     globals()[globalVarName] = xor(globals()[globalVarName], xor_op)
     return None
 
@@ -26,13 +33,54 @@ for i, opName in enumerate(opList):
     globals()[opName] = (lambda i: lambda: globalVarInPlaceXor('currentOpcode', genRightShiftString(i)))(i)
 
 def nextBeat():
-    global currentOpcode, defaultOpcode, genOpcode
+    global currentOpcode, defaultOpcode, genOpcode, currentBeats
     genOpcode.append(currentOpcode)
+    currentOpcode = defaultOpcode
+    currentBeats += 1
+    
+def endOfInstruction():
+    global currentBeats, currentOpcode, defaultOpcode
+    currentBeats = 0
     currentOpcode = defaultOpcode
     
 def reserve(num=1):
     global currentOpcode, defaultOpcode, genOpcode
+    assert(num >= 0)
     currentOpcode = defaultOpcode
     for i in xrange(num):
         nextBeat()
     
+def asm_instruction(f):
+    def wrapper(opLen = None):
+        global currentBeats
+        currentBeats = 0
+        f()
+        if opLen is not None:
+            reserve(opLen - currentBeats)
+        endOfInstruction()
+    return wrapper
+    
+def callInThisBeat(varList):
+    for i in varList:
+        globals()[i]()
+    nextBeat()
+    
+def convertToM19(opcodeList):
+    res = ''
+    n = len(opcodeList)
+    for i in xrange(0, n, 4):
+        numToPrint = min(4, n-i)
+        res += 'S1'
+        recordLen = numToPrint*4+3
+        res += "%02X" % recordLen
+        res += "%04X" % i
+        checksum = (0xff - recordLen) & 0xff
+        checksum = (checksum - ((i>>8) & 0xff)) & 0xff
+        checksum = (checksum - (i & 0xff)) & 0xff
+        for j in xrange(numToPrint):
+            res += opcodeList[i+j].upper()
+            for k in xrange(0, 8, 2):
+                checksum = (checksum - int(opcodeList[i+j][k:k+2], 16)) & 0xff
+        res += "%02X" % checksum
+        res += '\r\n'
+    return res + 'S9030000FC\r\n'
--- microOpcodeGen.py
+++ microOpcodeGen.py
@@ -1,25 +1,213 @@
 from microOp import *
     
 # DEFINE YOUR MICRO-OPS BELOW ======================
-def getOp():
+
+# some helper functions
+def getOpHelper():
+    '''
+    getOPs:
+    T0:
+    (PC) --MC=00--> AB;
+    (M) --CRDX=0--> DB --GI=0--> IR;
+    (PC)+1 --PINC=0--> PC;
+    (A) --GC=0--> ACT;
+    MPLD = 0;
+    1110 0000 0011 1111 1111 1111 1101 0111 = E0 3F FF D7
+    '''
     # T0:
+    callInThisBeat(['mxc0', 'mxc1', 'crdx', 'gi', 'pinc', 'gc', 'mpld'])
+
+
+def sendAddrH():
+    '''sendAddrH:
+    T1:
+    (PC) --MC=00--> AB;
+    (M) --CRDX=0--> DB --GA1=0--> ADRH;
+    (PC)+1 --PINC=0--> PC;
+    1110 1000 0011 1111 1111 1111 1011 1111 = E8 3F FF BF
+    '''
+    callInThisBeat(['mxc0', 'mxc1', 'crdx', 'ga1', 'pinc'])
+
+    
+def sendAddrL():
+    '''sendAddrL:
+    T1:
+    (PC) --MC=00--> AB;
+    (M) --CRDX=0--> DB --GA2=0--> ADRL;
+    (PC)+1 --PINC=0--> PC;
+    1110 1000 0011 1111 1111 1110 1111 1111 = E8 3F FE FF'''
+    callInThisBeat(['mxc0', 'mxc1', 'crdx', 'ga2', 'pinc'])
+
+    
+def sendAddr():
+    sendAddrH()
+    sendAddrL()
+    # T3: (ADR) --MC=01--> AB;
+    mxc1()
+
+def sendIR():
     mxc0()
     mxc1()
     crdx()
     gi()
-    pinc()
-    gc()
-    mpld()
     
-    # T1:
-    nextBeat()
+def getOpAtLast(f):
+    def wrapper():
+        f()
+        getOpHelper()
+    return wrapper
     
-    # T1 ~ T6: empty
-    reserve(6)
+# MICRO-OP BEGIN
+@asm_instruction
+def getOp():
+    getOpHelper()
+    
+@asm_instruction
+@getOpAtLast
+def LD():
+    '''(007)LD Ai, addr:
+    T1, T2: sendAddr;
+    T3:
+    (ADR) --MC=01--> AB;
+    (MEM) --CRDX=0--> DB --WRE=0--> Ai;
+    E0 3F FF D7
+    E8 3F FF BF
+    E8 3F FE FF
+    1110 1010 0111 1111 1111 1111 1111 1110 = EA 7F FF FE'''
+    sendAddr()
+    callInThisBeat(['crdx', 'wre'])
+    
+@asm_instruction
+@getOpAtLast
+def ST():
+    '''(00F)ST Ai, addr:
+    T1, T2: sendAddr;
+    T3:
+    (ADR) --MC=01--> AB;
+    (Ai) --MA=0,S=011--> ALU --MB=00--> DB --CWRX=0--> M;
+    E0 3F FF D7
+    E8 3F FF BF
+    E8 3F FE FF
+    1101 1010 0111 1001 1111 0011 1111 1111 = DA 79 F3 FF'''
+    sendAddr()
+    callInThisBeat(['mxa', 's2', 'mxb1', 'mxb0', 'cwrx'])
+
+@asm_instruction
+@getOpAtLast
+def MOV_R_data8():
+    '''
+    T1: 
+    (PC) --MC=00--> AB;
+    (M) --CRDX=0--> DB --WRE=0--> Ri;
+    (PC)+1 --PINC=0--> PC;'''
+    callInThisBeat(['mxc0', 'mxc1', 'crdx', 'wre', 'pinc'])
+    
+@asm_instruction
+@getOpAtLast
+def MOV_R_atR():
+    '''
+    T1:
+    (Rj) --MA=0, S=011--> ALU --MB=00--> DB --GA2=0--> ADRL;
+    7EH --AHS=0--> ADRH;
+    T2:
+    (ADR) --MC=01--> AB; (M) --CRDX=0--> DB --GT=0--> (TMP);
+    T3:
+    (PC) --MC=00--> AB; (M) --CRDX=0--> DB --GI=0--> IR;
+    (PC)+1 --PINC=0--> PC;
+    T4:
+    (TMP) --MA=1, S=011--> ALU --MB=00--> DB --WRE=0--> (Ri);
+    '''
+    callInThisBeat(['s2', 'mxa', 'mxb1', 'mxb0', 'ga2', 'ahs'])
+    callInThisBeat(['mxc1', 'crdx', 'gt'])
+    callInThisBeat(['mxc0', 'mxc1', 'crdx', 'gi', 'pinc'])
+    callInThisBeat(['s2', 'mxb0' ,'mxb1', 'wre'])
+
+@asm_instruction
+@getOpAtLast
+def MOV_atR_R():
+    '''
+    T1:
+    (Ri) --MA=0, S=011--> ALU --MB=00--> DB --GT=0--> (TMP);
+    T2:
+    (PC) --MC=00--> AB; (M) --CRDX=0--> DB --GI=0--> IR;
+    (PC)+1 --PINC=0--> PC;
+    T3:
+    (Rj) --MA=0, S=011--> ALU --MB=00--> DB --GA2=0--> ADRL;
+    7EH --AHS=0--> ADRH;
+    T4:
+    (ADR) --MC=01--> AB; 
+    (TMP) --MA=1, S=011--> ALU --MB=00--> DB --CWRX=0--> (M);
+    '''
+    callInThisBeat(['mxa', 's2', 'mxb1', 'mxb0', 'gt'])
+    callInThisBeat(['mxc0', 'mxc1', 'crdx', 'gi', 'pinc'])
+    callInThisBeat(['mxa', 's2', 'mxb1', 'mxb0', 'ga2', 'ahs'])
+    callInThisBeat(['mxc1', 's2', 'mxb1', 'mxb0', 'cwrx'])
+
+@asm_instruction
+@getOpAtLast
+def ADD_R_R():
+    '''
+    T1:
+    (Rj) --MA=0, S=011--> ALU --MB=00--> DB --GA=0--> (A);
+    (PC) --MC=00--> AB;
+    T2:
+    (M) --CRDX=0--> DB --GI=0--> IR;
+    (PC)+1 --PINC=0--> (PC);
+    (A) --GC=O--> ACT;
+    T3:
+    (Ri)+ACT --MA=0, S=000--> ALU --MB=00--> DB --WRE=0--> (Ri)
+    '''
+    callInThisBeat(['mxa','s2','mxb1','mxb0','ga','mxc1','mxc0'])
+    callInThisBeat(['crdx','gi','pinc','gc'])
+    callInThisBeat(['mxa','s2','s1','s0','mxb1','mxb0','wre'])
+    
+@asm_instruction
+@getOpAtLast
+def SUB_R_R():
+    '''
+    T1:
+    (Rj) --MA=0, S=011--> ALU --MB=00--> DB --GA=0--> (A);
+    (PC) --MC=00--> AB;
+    T2:
+    (M) --CRDX=0--> DB --GI=0--> IR;
+    (PC)+1 --PINC=0--> (PC);
+    (A) --GC=O--> ACT;
+    T3:
+    (Ri)-ACT --MA=0, S=110--> ALU --MB=00--> DB --WRE=0--> (Ri)
+    '''
+    callInThisBeat(['mxa','s2','mxb1','mxb0','ga','mxc1','mxc0'])
+    callInThisBeat(['crdx','gi','pinc','gc'])
+    callInThisBeat(['mxa','s0','mxb1','mxb0','wre'])
+    
     
 # list your asm instructions
-getOp()
+getOp(7)
+LD(8)
+ST(8)
+MOV_R_data8(8)
+MOV_R_atR(8)
+MOV_atR_R(8)
+ADD_R_R(8)
+SUB_R_R(8)
+'''JMP_addr(8)
+JC_rel8(8)
+CALL_addr(8)
+RET(8)
+PUSH_R(8)
+POP_R(8)
+SUB_R_atR(8)
+XOR_R_data(8)
+NEG_addr(8)
+JLS_rel8(8)
+JNZ_rel8(8)
+LSR_R(8)'''
     
 # print the final bytes of the micro programs
-print [i.encode('hex') for i in genOpcode]
+genOpcode = [i.encode('hex') for i in genOpcode]
+print len(genOpcode), genOpcode
+
+genOpcode = convertToM19(genOpcode)
+print genOpcode
     
+with open('../genCode.m19','wb') as f:
+    f.write(genOpcode)
