bba36975a46997599d0a4b6d98f562340146d077
garzon <610204679@qq.com>
2016-04-05 13:52:39 +0800

    bugfix

--- microOpcodeGen.py
+++ microOpcodeGen.py
@@ -47,15 +47,19 @@ def calcRel8():
     (PCH) --MB=01, OB=0--> DB --GA1=0--> ADRH;
     T2:
     (PCL) --MB=10, OB=0--> DB --GC=0--> ACT;
-    T3:
+    T3: 
+    (ACT)+1 --S=110--> ALU --MB=00, OB=0--> DB --GC=0--> ACT;
+    T4:
     (PC) --MC=00--> AB;
     (M) --CRDX=0--> DB --GT=0--> TMP;
-    T4:
+    (PC)+1 --PINC=0--> PC;
+    T5:
     ACT+TMP --S=000--> ALU --MB=00, OB=0--> DB --GA2=0--> ADRL;
     '''
     callInThisBeat(['mxb1', 'ob', 'ga1'])
     callInThisBeat(['mxb0', 'ob', 'gc'])
-    callInThisBeat(['mxc0', 'mxc1', 'crdx', 'gt'])
+    callInThisBeat(['s0', 'mxb1', 'mxb0', 'ob', 'gc'])
+    callInThisBeat(['mxc0', 'mxc1', 'crdx', 'gt', 'pinc'])
     callInThisBeat(['s2', 's1', 's0', 'mxb1', 'mxb0', 'ob', 'ga2'])
     
 def sendAddr():
@@ -189,8 +193,8 @@ def JMP_addr():
 @getOpAtLast
 def JC_rel8():
     '''JC rel8
-    T1, T2, T3, T4: calcRel8()
-    T5:
+    T1, T2, T3, T4, T5: calcRel8()
+    T6:
     (ADR) --MC=01--> AB;
     AB --PLD210=001--> PC;
     '''
@@ -213,8 +217,8 @@ def CALL_addr():
     T5:
     (ADR) --MC=01--> AB --PLD210=101--> PC;'''
     sendAddr()
-    callInThisBeat(['mxc0', 'mxb1', 'cwrx', 'ssp1', 'ob'])
-    callInThisBeat(['mxc0', 'mxb0', 'cwrx', 'ssp1', 'ob'])
+    callInThisBeat(['mxc0', 'mxb1', 'cwrx', 'ssp0', 'ob'])
+    callInThisBeat(['mxc0', 'mxb0', 'cwrx', 'ssp0', 'ob'])
     callInThisBeat(['mxc1', 'pld1'])
     
 @asm_instruction
@@ -231,8 +235,8 @@ def RET():
     (M) --CRDX=0--> DB --GA1=0--> ADRH;
     T4:
     (ADR) --MC=01--> AB --PLD210=101--> PC;'''
-    callInThisBeat(['ssp0'])
-    callInThisBeat(['mxc0', 'crdx', 'ga2', 'ssp0'])
+    callInThisBeat(['ssp1'])
+    callInThisBeat(['mxc0', 'crdx', 'ga2', 'ssp1'])
     callInThisBeat(['mxc0', 'crdx', 'ga1'])
     callInThisBeat(['mxc1', 'pld1'])
     
@@ -245,7 +249,7 @@ def PUSH():
     (Ai) --MB=11, OB=0--> DB --CWRX=0--> M;
     (SP)-1 --SSP=01--> SP;
     '''
-    callInThisBeat(['mxc0', 'ob', 'cwrx', 'ssp1'])
+    callInThisBeat(['mxc0', 'ob', 'cwrx', 'ssp0'])
     
 @asm_instruction
 @getOpAtLast
@@ -257,7 +261,7 @@ def POP():
     (SP) --MC=10--> AB;
     (M) --CRDX=0--> DB --WRE=0--> (Ai);
     '''
-    callInThisBeat(['ssp0'])
+    callInThisBeat(['ssp1'])
     callInThisBeat(['mxc0', 'crdx', 'wre'])
     
 @asm_instruction
@@ -320,8 +324,8 @@ def NEG_addr():
 @getOpAtLast
 def JLS_rel8():
     '''JLS rel8
-    T1, T2, T3, T4: calcRel8()
-    T5:
+    T1, T2, T3, T4, T5: calcRel8()
+    T6:
     (ADR) --MC=01--> AB;
     AB --PLD210=000--> PC;
     '''
@@ -332,8 +336,8 @@ def JLS_rel8():
 @getOpAtLast
 def JNZ_rel8():
     '''JNZ rel8
-    T1, T2, T3, T4: calcRel8()
-    T5:
+    T1, T2, T3, T4, T5: calcRel8()
+    T6:
     (ADR) --MC=01--> AB;
     AB --PLD210=010--> PC;
     '''
@@ -356,8 +360,8 @@ def LSR():
 @getOpAtLast
 def JNKB_rel8():
     '''JNKB rel8
-    T1, T2, T3, T4: calcRel8()
-    T5:
+    T1, T2, T3, T4, T5: calcRel8()
+    T6:
     (ADR) --MC=01--> AB;
     AB --PLD210=011--> PC;
     '''
@@ -368,8 +372,8 @@ def JNKB_rel8():
 @getOpAtLast
 def JNPB_rel8():
     '''JNPB rel8
-    T1, T2, T3, T4: calcRel8()
-    T5:
+    T1, T2, T3, T4, T5: calcRel8()
+    T6:
     (ADR) --MC=01--> AB;
     AB --PLD210=100--> PC;
     '''
@@ -393,6 +397,15 @@ def CMP_A_A():
     sendIR()
     callInThisBeat(['ob', 'gc'])
     callInThisBeat(['s2', 's1', 'cp', 'zp'])
+    
+@asm_instruction
+@getOpAtLast
+def INIT():
+    '''INIT
+    T1:
+    7FFFH --SSP=11--> SP;
+    '''
+    callInThisBeat(['ssp0', 'ssp1'])
     
 # list your asm instructions
 getOp(7)
@@ -419,7 +432,7 @@ LSR(8)
 JNKB_rel8(8)
 JNPB_rel8(8)
 CMP_A_A(8)
-
+INIT(8)
     
 print '================================================'
 
